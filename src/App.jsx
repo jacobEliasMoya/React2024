33f8883fbc2494c9de958025148ead61914c055c
jacob moya <jemoya1994@Gmail.com>
2024-05-21 19:23:03 -0700

    first commit in a while

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Header from "./header";
+
+function App() {
+  return(
+    <Header />
+  );
+}
+
+export default App
---
+++ src/header.jsx
@@ -0,0 +1,16 @@
+function Header(){
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Reviews</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
